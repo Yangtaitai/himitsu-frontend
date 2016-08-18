916ffcb5c7e03c58d14e5b49e4105b899f5e40b2
Yangtaitai <amyfan@gwmail.gwu.edu>
2016-08-18 16:56:01 -0400

    ddd

--- services/userService.js
+++ services/userService.js
@@ -2,90 +2,87 @@
 
     angular
         .module('himitsuApp')
-        .factory('userService', function ($http, $HOST,$localStorage) {
-            
+        .factory('userService', function ($http, $HOST, $localStorage) {
+
             var userService = this;
 
             userService.userId = null;
 
             userService.postLogin = function (data) {
                 console.log(data);
-                
-                return $http.post($HOST.url+'/login', data)
-                .then(function(res){
-                    console.log(res.data.data.id);
-                    
-                    if(res.data){
 
-                        userService.userId = res.data.data.id;
+                return $http.post($HOST.url + '/login', data)
+                    .then(function (res) {
 
-                        console.log('localStorage');
-                        
+                        if (res.data) {
 
-                        $localStorage.userId = res.data.data.id;
+                            if (res.data.result) {
 
-                        console.log($localStorage.userId);
+                                userService.userId = res.data.data.id;
+                                $localStorage.userId = res.data.data.id;
 
-                        return res.data;
-                        
-                    }else{
-                        return {result: false};
-                    }
-                });
-                
+                            }
+
+                            return res.data;
+                            
+                        } else {
+
+                            return { result: false };
+                        }
+                    });
             };
 
             userService.postSignUp = function (data) {
-                
+
                 return $http.post($HOST.url + '/user', data)
-                    .then(function(res){
+                    .then(function (res) {
 
                         console.log('service');
-                        
+
                         console.log(res.data);
 
                         console.log('Sign up post!!!');
 
-                        if(res.data){
+                        if (res.data) {
                             console.log(res.data);
                             return res.data;
-                            
-                        }else{
-                            return{
-                                result:false
+
+                        } else {
+                            return {
+                                result: false
                             };
                         };
                     });
             };
 
-            userService.loadUser = function(){
-                return $http.get($HOST.url+'/user/'+$localStorage.userId)
-                    .then(function(res){
-                        if(res.data){
-                            if(res.data.result){
+            userService.loadUser = function () {
+                return $http.get($HOST.url + '/user/' + $localStorage.userId)
+                    .then(function (res) {
+                        if (res.data) {
+                            if (res.data.result) {
                                 $localStorage.user = res.data.data;
                             }
 
                             return res.data;
-                        }else{
-                            return {result:false};
+                        } else {
+                            return { result: false };
                         }
                     })
             };
 
 
-            userService.getUserId = function(){
+            userService.getUserId = function () {
                 return $localStorage.userId;
             };
-            
-            userService.getUser = function(){
+
+            userService.getUser = function () {
                 return $localStorage.user;
             };
 
             userService.searchUserByFirstName = function (keyword) {
                 if (!keyword)
                     keyword = "";
-                return $http.get($HOST.url+'/user?firstName=' + keyword)
+                return $http.get($HOST.url + '/user?firstName=' + keyword)
                     .then(function (res) {
 
                         if (res.data)
